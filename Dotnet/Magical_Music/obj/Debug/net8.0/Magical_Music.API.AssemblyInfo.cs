a67a0a650b89ff6567cb72e6e23d9901849734e9
EfratAvigail <f0548573828@gmail.com>
2025-05-30 00:44:59 +0300

    Docker8

--- Dotnet/Magical_Music/obj/Debug/net8.0/Magical_Music.API.AssemblyInfo.cs
+++ Dotnet/Magical_Music/obj/Debug/net8.0/Magical_Music.API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Magical_Music.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaec0321aa5bccda6e4e6dbd5b4a9c8b93bba9a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d20ada94fc140e176dec287db09c10806311cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Magical_Music.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Magical_Music.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
